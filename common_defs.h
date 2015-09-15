3048d256b735e7149af8982f57cd01587d8583fc
bboygus <gas-o@hotmail.it>
2015-09-15 15:02:20 +0200

    Added the common definitions

---
+++ common_defs.h
@@ -0,0 +1,12 @@
+/* Standard headers */
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+/* openCV headers */
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgproc.hpp>
