a702bc7128f84e238721231ca2f9fe2c5433cc84
CiottiGiorgio <ciotti.giorgio.96@gmail.com>
2015-09-16 18:30:57 +0200

    added some files

--- common_defs.h
+++ common_defs.h
@@ -10,3 +10,8 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/objdetect.hpp>
 #include <opencv2/imgproc.hpp>
+
+/* Wrapper TYPES */
+#ifndef TYPES
+enum TYPES = {PEDESTRIANS, CYCLES, CARS};
+#endif
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "detector.h"
+
+int main ()
+{
+   Detector detector(PEDESTRIANS);
+   cv::VideoCapture vc;
+   cv::UMat frame;
+   cv::Mat img_to_show;
+
+   vc.open("/home/ubuntu/development/pedestrian/768x576.avi");
+
+   while (true)
+   {
+      vc >> frame;
+      frame.copyTo(img_to_show);
+
+      std::vector<cv::Rect> found;
+
+      //detection of pedestrians
+      found = detector.detect(frame);
+
+      // Draw positive classified windows
+      for (size_t i = 0; i < found.size(); i++)
+      {
+         cv::Rect r = found[i];
+         rectangle(img_to_show, r.tl(), r.br(), cv::Scalar(0, 255, 0), 3);
+      }
+
+      //realtime output of elaborated frame
+      imshow("debug", frame);
+      cv::waitKey(3);
+
+   }
+
+   return 0;
+}
